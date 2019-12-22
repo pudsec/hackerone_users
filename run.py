b86112194197a916e22977d2c9e7b94a10fae168
pudsec <pudsec@gmail.com>
2019-12-22 21:00:26 +0800

    update

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ import string
 with open('users.csv', 'w') as f:
     c = csv.writer(f, delimiter=',', lineterminator='\n')
     for s in string.digits + string.ascii_lowercase:
-        s = requests.get(
+        r = requests.get(
             'https://hackerone.com/sitemap?first=' + s,
             headers={
                 'Accept': 'application/json',
                 'X-Requested-With': 'XMLHttpRequest',
             },
         ).json()['users']
-        for u in s:
+        for u in r:
             c.writerow([u['name'], u['username'], 'https://hackerone.com/' + u['username']])
